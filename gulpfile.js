7210b1efba04c6be5a6ecba56a00291971682b97
avnikasikciwin <avni.kasikci.01@gmail.com>
2022-10-22 21:19:26 +0300

    delete: unneccary files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('default',defaultTask);
-
-function defaultTask(done){
-    console.log("hello gulp has been integred into your projects");
-    done();
-}
--- index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-//module.exports = require('./lib/gulp-copy');
-module.exports = require('gulp-copy');
